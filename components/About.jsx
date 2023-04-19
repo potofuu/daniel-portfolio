0003b72348fcaf8d9e69d7027a3caef1d84d67af
Daniel Duan <daniel.duan03@gmail.com>
2023-04-18 21:26:55 -0400

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -9,8 +9,9 @@ const About = () => {
             <div className='col-span-2'>
                 <p className='uppercase text-xl tracking-widest text-[skyblue] font-bold'>About Me</p>
                 <h2 className='py-4'>Who I Am</h2>
-                <p className='py-2 text-gray-500'>I am currently a first year student at Georgia Tech, studying CS with threads(focus) in intelligence and media.</p>
-                <p className='py-2 text-gray-500'>I am interested in learning about AI/machine learning, data science, and web development. In my free time, I enjoy playing tennis, games and chess.</p>
+                <p className='py-2 text-gray-500'>I am currently a first year student at Georgia Tech, studying CS with a focus in people and media.</p>
+                <p className='py-2 text-gray-500'>I am most interested in fullstack development, and love learning about all kinds of technologies.</p>
+                <p className='py-2 text-gray-500'>In my free time, I enjoy playing tennis, games and chess.</p>
                 <p><br></br></p>
                 <Link href='/#projects'>
                     <p className='py-2 text-gray-500 hover:underline cursor-pointer'>Check out my projects here!</p>
